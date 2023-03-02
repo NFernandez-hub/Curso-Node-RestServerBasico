a4ac4494a34e8bd82e05ae568aca78a8cf689775
Nicolas Fernandez <nfernandez.dev@gmail.com>
2023-03-02 18:58:07 -0300

    Fin seccion 9

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,22 +1,42 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { roleValidator, emailValidator, userExistById } = require('../helpers/db-validators');
+const { validarCampos } = require('../middlewares/validar-campos');
+
 const { getUsuarios,
     deleteUsuarios,
     postUsuarios,
     putUsuarios,
     patchUsuarios } = require('../controllers/usuarios');
 
-
 const router = Router();
 
 router.get('/', getUsuarios)
 
-router.post('/', postUsuarios)
+router.put('/:id',[
+    check('id', 'No es un ID valido ').isMongoId(),
+    check('id').custom(userExistById),
+    check('role').custom(roleValidator),
+    validarCampos
+], putUsuarios)
 
-router.put('/', putUsuarios)
+router.post('/', [
+    check('name', 'El nombre no es valido').not().isEmpty(),
+    check('password', 'La contraseña debe tener mas de 6 caracteres').isLength({ min: 6 }),
+    check('email', 'El email no es valido').isEmail(),
+    check('email').custom(emailValidator),
+    check('role').custom(roleValidator),
+    validarCampos
+], postUsuarios)
 
 router.patch('/', patchUsuarios)
 
-router.delete('/', deleteUsuarios)
+router.delete('/:id',[
+    check('id', 'No es un ID valido ').isMongoId(),
+    check('id').custom(userExistById),
+    validarCampos
+], deleteUsuarios)
 
 
 module.exports = router;
